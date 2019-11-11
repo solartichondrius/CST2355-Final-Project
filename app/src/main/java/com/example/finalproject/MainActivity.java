ffca58d8a5241b0bd1899e63219472fbc40edd80
Thomas <curt0053@algonquinlive.com>
2019-11-11 17:27:50 -0500

    added everyone elses buttons to the main activity

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -13,7 +13,28 @@ public class MainActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        /*
+        Button carButton = findViewById(R.id.carButton);
+        if(carButton != null)
+            carButton.setOnClickListener( v -> {
+                Intent goToCarActivity = new Intent(getApplicationContext(), CarActivity.class);
+                startActivity(goToCarActivity);
+            });
 
+        Button recipeButton = findViewById(R.id.recipeButton);
+        if(recipeButton != null)
+            recipeButton.setOnClickListener( v -> {
+                Intent goToRecipeActivity = new Intent(getApplicationContext(), RecipeActivity.class);
+                startActivity(goToRecipeActivity);
+            });
+
+        Button currencyButton = findViewById(R.id.currencyButton);
+        if(currencyButton != null)
+            currencyButton.setOnClickListener( v -> {
+                Intent goToCurrencyActivity = new Intent(getApplicationContext(), CurrencyActivity.class);
+                startActivity(goToCurrencyActivity);
+            });
+        */
         Button newsButton = findViewById(R.id.newsButton);
         if(newsButton != null)
             newsButton.setOnClickListener( v -> {
