1f4b2b854d3ddda53e6fdc9e4b9db583c9a7ea2a
School <king0455@algonquinlive.com>
2019-11-11 18:40:47 -0500

    changed styles; wouldn't load edited search layout edited SearchActivity

--- app/src/main/java/com/example/chargingstation/SearchActivity.java
+++ app/src/main/java/com/example/chargingstation/SearchActivity.java
@@ -2,6 +2,8 @@ package com.example.chargingstation;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.finalproject.*;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class SearchActivity extends AppCompatActivity {
@@ -10,5 +12,22 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    public class SearchQuery extends AsyncTask <String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... args){
+
+            return null;
+        }
+
+        public void onProgressUpdate(Integer... value){
+
+        }
+
+        public void onPostExecute(String value){
+
+        }
     }
 }
