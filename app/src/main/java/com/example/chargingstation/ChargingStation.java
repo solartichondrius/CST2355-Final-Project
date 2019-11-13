a742030eb120ee8ea48c13f8711f22de38dd4687
School <king0455@algonquinlive.com>
2019-11-12 20:27:30 -0500

    Worked on SearchActivity. Added new class ChargingStation. Added new layout file for listview.

---
+++ app/src/main/java/com/example/chargingstation/ChargingStation.java
@@ -0,0 +1,45 @@
+package com.example.chargingstation;
+
+public class ChargingStation {
+
+    private String title;
+    private double longitude;
+    private double latitude;
+    private String phone;
+
+    public ChargingStation(String title, double longitude, double latitude, String phone){
+        this.title = title;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.phone = phone;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setLongitude(double longitude){
+        this.longitude = longitude;
+    }
+    public double getLongitude(){
+        return longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
--- app/src/main/java/com/example/chargingstation/SearchActivity.java
+++ app/src/main/java/com/example/chargingstation/SearchActivity.java
@@ -3,12 +3,19 @@ package com.example.chargingstation;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.finalproject.*;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -22,11 +29,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
     String longitude;
     String latitude;
+    EditText longitudeText;
+    EditText latitudeText;
+    SharedPreferences sharedPrefs;
+    ArrayList<ChargingStation> stations = new ArrayList<>();
+    BaseAdapter myAdapter;
+    View thisRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +48,50 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         Button searchButton = findViewById(R.id.searchActivityButton);
+
+        longitudeText = findViewById(R.id.searchLongitude);
+        longitude = longitudeText.getText().toString();
+        latitudeText = findViewById(R.id.searchLatitude);
+        latitude = latitudeText.getText().toString();
+
+        sharedPrefs = getSharedPreferences("SavedCoordinates", Context.MODE_PRIVATE);
+        String previousLong = sharedPrefs.getString("searchLongitude", longitude);
+        longitudeText.setText(previousLong);
+
+        View progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        ListView list = findViewById(R.id.searchResults);
+        myAdapter = new MyListAdapter();
+        list.setAdapter(myAdapter);
+        list.setOnItemClickListener((lv, vw, pos, id) -> {
+            ChargingStation station = stations.get(pos);
+            
+        });
+
         searchButton.setOnClickListener(e -> {
-            EditText longitudeText = findViewById(R.id.searchLongitude);
-            longitude = longitudeText.getText().toString();
-            EditText latitudeText = findViewById(R.id.searchLatitude);
-            latitude = latitudeText.getText().toString();
             SearchQuery searchQuery = new SearchQuery();
             searchQuery.execute();
         });
+
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString("longitudeSearch", longitude);
+        editor.commit();
     }
 
     public class SearchQuery extends AsyncTask <String, Integer, String> {
 
         String title;
         String phone;
-        String resultLong;
-        String resultLat;
+        double resultLong;
+        double resultLat;
 
         @Override
         protected String doInBackground(String... args){
@@ -73,38 +115,82 @@ public class SearchActivity extends AppCompatActivity {
 
                 for(int i = 0; i < resultArray.length(); i++){
                     JSONObject result = resultArray.getJSONObject(i);
-                    JSONObject operatorObject = result.getJSONObject("OperatorInfo");
-                    if(operatorObject != null){
+                    if(!result.isNull("OperatorInfo")) {
+                        JSONObject operatorObject = result.getJSONObject("OperatorInfo");
                         String titleTemp = operatorObject.getString("Title");
-                        if(titleTemp != null){
+                        if (titleTemp != null) {
                             title = titleTemp;
-                        }else {
+                        } else {
                             title = "No name";
                         }
+                        String phoneTemp = operatorObject.getString("PhonePrimaryContact");
+                        if(phoneTemp != null){
+                            phone = phoneTemp;
+                        }else {
+                            phone = "No phone number";
+                        }
+
                     }else {
                         title = "No name";
+                        phone = "No phone number";
                     }
-                        Log.e("LOOK HERE", title);
+                    if(!result.isNull("AddressInfo")){
+                        JSONObject addressObject = result.getJSONObject("AddressInfo");
+                        resultLong = addressObject.getDouble("Longitude");
+                        resultLat = addressObject.getDouble("Latitude");
+                    }else {
+                        resultLong = 0;
+                        resultLat = 0;
+                    }
 
+                    stations.add(new ChargingStation(title, resultLong, resultLat, phone));
                 }
 
             }catch(MalformedURLException mfe){
                 ret = "Malformed URL exception";
+                Log.e(ret, mfe.getMessage());
             }catch(IOException ioe){
                 ret = "IO Exception. Is the wifi connected?";
+                Log.e(ret, ioe.getMessage());
             } catch (JSONException e) {
                 ret = "JSON issues";
+                Log.e(ret, e.getMessage());
             }
 
             return ret;
         }
 
         public void onProgressUpdate(Integer... value){
-
+            ProgressBar progressBar = findViewById(R.id.progressBar);
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.setProgress(value[0]);
         }
 
         public void onPostExecute(String value){
+            myAdapter.notifyDataSetChanged();
+        }
+    }
 
+    private class MyListAdapter extends BaseAdapter{
+        @Override
+        public int getCount(){return stations.size();}
+        @Override
+        public ChargingStation getItem(int position){return stations.get(position);}
+        @Override
+        public long getItemId(int position){return position;}
+        @Override
+        public View getView(int position, View recycled, ViewGroup parent){
+            thisRow = recycled;
+
+            ChargingStation station = getItem(position);
+
+            if(recycled == null){
+                thisRow = getLayoutInflater().inflate(R.layout.list_chargingsearch, null);
+            }
+            TextView titleResult = thisRow.findViewById(R.id.chargingTitle);
+            titleResult.setText(station.getTitle());
+
+            return thisRow;
         }
     }
 }
